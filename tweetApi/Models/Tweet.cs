b87d00d62c3266af27a35bd873d78b6fdc951f4e
Ankit Srivastava <ankitsrivastava.fsd@gmail.com>
2021-12-16 20:21:06 +0530

    Map validation attributes

--- tweetApi/Models/Tweet.cs
+++ tweetApi/Models/Tweet.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace tweetApi
 {
     public class Tweet
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(144)]
         public string Message { get; set; }
         public string Image { get; set; }
         public string Name { get; set; }
+        [Required]
         public DateTime TimeStamp { get; set; }
         public IEnumerable<Tweet> Reply { get; set; }
-        public int ParentId { get; set; }
+        public int? ParentId { get; set; }
+        [Required]
         public string UserName { get; set; }
         public IEnumerable<User> LikesBy { get; set; }
 
--- tweetApi/Models/User.cs
+++ tweetApi/Models/User.cs
@@ -1,11 +1,18 @@
-﻿namespace tweetApi
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tweetApi
 {
     public class User
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
         public string EmailId { get; set; }
+        [Required]
         public string UserName { get; set; }
         public string Image { get; set; }
     }
