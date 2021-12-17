1ec970fe776119aef6d7fab5723a57cb00a0c12e
Ankit Srivastava <ankitsrivastava.fsd@gmail.com>
2021-12-17 19:46:22 +0530

    Added EntityFramework Inmemory database in the project

---
+++ DataSource/TweetAppContext.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSource
+{
+    public class TweetAppContext : DbContext
+    {
+        public TweetAppContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Tweet> Tweets;
+        public DbSet<User> Users;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tweet>()
+                .HasMany(t => t.LikesBy);
+
+
+            ///Seeding
+            ///
+            modelBuilder.Entity<User>().HasData(
+                new User { Id=1 ,FirstName = "Test User ", LastName = "Last Name", EmailId = "test@test.com", UserName = "testUser", Password = "Test", ConfirmPassword = "Test" },
+                new User { Id = 2,FirstName = "Test Second User ", LastName = "Last Name", EmailId = "test2@test.com", UserName = "testUser2", Password = "Test", ConfirmPassword = "Test" }
+                ) ;
+            modelBuilder.Entity<Tweet>().HasData(
+                    new Tweet { Id=1, UserName="testUser",  LikesBy=new List<User>(),Message="Hello Seeded Data ", Name = "Test User Last Name", ParentId= null, TimeStamp= DateTime.Now}
+                );
+        }
+    }
+}
--- tweetApi/Startup.cs
+++ tweetApi/Startup.cs
@@ -1,5 +1,7 @@
+using DataSource;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,7 +21,10 @@ namespace tweetApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TweetAppContext>(option =>
+            {
+                option.UseInMemoryDatabase("TweetApp");
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -28,7 +33,7 @@ namespace tweetApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, TweetAppContext tweetAppContext)
         {
             if (env.IsDevelopment())
             {
@@ -36,7 +41,8 @@ namespace tweetApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "tweetApi v1"));
             }
-
+            tweetAppContext.Database.EnsureDeleted();
+            tweetAppContext.Database.EnsureCreated();
             app.UseHttpsRedirection();
 
             app.UseRouting();
